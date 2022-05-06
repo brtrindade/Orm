bc6111c5251514eb3f198b288ffa2471da242cdc
Ilso <ilso_1000@hotmail.com>
2022-05-06 12:00:26 -0300

    commit all

---
+++ db/api/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: '19pk7i24I!',
+  database: 'escola_ingles',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
